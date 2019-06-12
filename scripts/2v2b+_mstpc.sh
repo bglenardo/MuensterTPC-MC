fc1e0184df20ed837f34648883a5f1627f2415e4
Christian W <c.wittweg@wwu.de>
2019-06-12 13:28:38 +0200

    Add decay0 files for b+b+
    Added:
    - decay0 files 2vb+b+
    - decay 0 viles for 0vb+b+ (both mn and rc options)
    - macros
    - scripts
    - example root files with 10000 events
    
    Changed:
    - macros to generate events in full LXe volume

---
+++ scripts/2v2b+_mstpc.sh
@@ -0,0 +1,27 @@
+# Shell script to run the MS-TPC Monte Carlo using a Decay0 macro
+
+# Your bashrc command aliases to load root and geant4
+load_geant4
+load_root6
+
+echo 'Loaded ROOT6 and Geant4.10'
+
+# Specify macro/file-name and directory to save files in
+filename='Decay0_Xe124_2nu2bplus'
+filedir='/Users/christianwittweg/Desktop/0vECEC/'
+
+echo 'Filename is '$filename
+
+cd $G4WORKDIR/MuensterTPC-MC
+
+#Run the MC, change verbosity for more info with '-v 1'
+runstring='./MuensterTPC-MC -f ./macros/src_'$filename'.mac -o '$filename'.root -n 10000 -v 0'
+
+$runstring
+
+# Move files and go to filedir
+mv *.root $filedir
+
+echo 'Moving ROOT-file to the working directory'
+
+#cd $filedir
